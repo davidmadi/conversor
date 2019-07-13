5ea05cf54157612f66b9710b55897913a22dbfb4
David Madi <davidmadi@Davids-MacBook-Pro.local>
2019-07-13 18:29:09 -0300

    reset

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import conversorReducer from './conversor';
-import languageReducer from './language';
-
-export default combineReducers({
-  conversorReducer: conversorReducer,
-  languageReducer : languageReducer
-});
